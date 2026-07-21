f1513798693921a9f5ef570d28ba3f8776daa2a7
YairArtz <ynegativo5@gmail.com>
2026-07-21 16:14:32 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 //  CHOQUE DE HÉROES TCG — Service Worker v6
 //  Con notificaciones locales de noticias y torneos
 // ================================================================
-const CACHE_NAME    = 'chh-tcg-v22';
-const CACHE_DYNAMIC = 'chh-dynamic-v22';
+const CACHE_NAME    = 'chh-tcg-v23';
+const CACHE_DYNAMIC = 'chh-dynamic-v23';
 
 const CACHE_CORE = [
   './', './boot.html', './index.html', './calculadora.html',
